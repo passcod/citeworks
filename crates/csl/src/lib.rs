ddfc0a83d8973a47165ffe2e213b9fe2896a627b
Félix Saparelli <felix@passcod.name>
2022-08-13 02:35:57 +1200

    Add json read/write functions

--- crates/csl/src/lib.rs
+++ crates/csl/src/lib.rs
@@ -2,7 +2,65 @@
 //!
 //! This targets CSL 1.0.2 but may not support the entire specification.
 
+use std::io::{Read, Write};
+
+use serde_json::Result;
+
+pub use items::Item;
+
 pub mod dates;
 pub mod items;
 pub mod names;
 pub mod ordinaries;
+
+/// Deserialize CSL items from an IO stream of JSON.
+pub fn from_reader<R>(rdr: R) -> Result<Vec<Item>>
+where
+	R: Read,
+{
+	serde_json::from_reader(rdr)
+}
+
+/// Deserialize CSL items from bytes of JSON text.
+pub fn from_slice<'a>(v: &'a [u8]) -> Result<Vec<Item>> {
+	serde_json::from_slice(v)
+}
+
+/// Deserialize CSL items from a string of JSON text.
+pub fn from_str<'a>(s: &'a str) -> Result<Vec<Item>> {
+	serde_json::from_str(s)
+}
+
+/// Serialize the given CSL items as a String of JSON.
+pub fn to_string(value: &[Item]) -> Result<String> {
+	serde_json::to_string(value)
+}
+
+/// Serialize the given CSL items as a pretty-printed String of JSON.
+pub fn to_string_pretty(value: &[Item]) -> Result<String> {
+	serde_json::to_string_pretty(value)
+}
+
+/// Serialize the given CSL items as a JSON byte vector.
+pub fn to_vec(value: &[Item]) -> Result<Vec<u8>> {
+	serde_json::to_vec(value)
+}
+
+/// Serialize the given CSL items as a pretty-printed JSON byte vector.
+pub fn to_vec_pretty(value: &[Item]) -> Result<Vec<u8>> {
+	serde_json::to_vec_pretty(value)
+}
+
+/// Serialize the given CSL items as JSON into the IO stream.
+pub fn to_writer<W>(writer: W, value: &[Item]) -> Result<()> where
+    W: Write
+{
+	serde_json::to_writer(writer, value)
+}
+
+/// Serialize the given CSL items as pretty-printed JSON into the IO stream.
+pub fn to_writer_pretty<W>(writer: W, value: &[Item]) -> Result<()> where
+    W: Write
+{
+	serde_json::to_writer_pretty(writer, value)
+}
