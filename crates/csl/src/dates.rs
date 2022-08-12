df976b70b3f231ff799ff253f9e92af8d44c19a3
Félix Saparelli <felix@passcod.name>
2022-08-13 02:12:47 +1200

    Implement custom serde for the complex date value

--- crates/csl/src/dates.rs
+++ crates/csl/src/dates.rs
@@ -2,7 +2,8 @@
 
 use std::collections::HashMap;
 
-use serde::{Deserialize, Serialize};
+use serde::de::Error;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::ordinaries::OrdinaryValue;
 
@@ -27,7 +28,7 @@ use crate::ordinaries::OrdinaryValue;
 ///
 /// [EDTF]: https://www.librarianshipstudies.com/2016/05/extended-date-time-format-edtf.html
 /// [meta]: DateMeta
-#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum DateValue {
 	/// Structured single date
 	Single { date: Date, meta: DateMeta },
@@ -44,6 +45,110 @@ pub enum DateValue {
 
 	/// EDTF
 	Edtf { date: String, meta: DateMeta },
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct DateInternal {
+	#[serde(skip_serializing_if = "Vec::is_empty")]
+	date_parts: Vec<Date>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	season: Option<Season>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	circa: Option<Circa>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	literal: Option<String>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	raw: Option<String>,
+
+	#[serde(skip_serializing_if = "Option::is_none")]
+	edtf: Option<String>,
+
+	#[serde(flatten)]
+	extra: HashMap<String, OrdinaryValue>,
+}
+
+impl DateValue {
+	pub fn meta(&self) -> &DateMeta {
+		match self {
+			Self::Single { meta, .. }
+			| Self::Range { meta, .. }
+			| Self::Raw { meta, .. }
+			| Self::Edtf { meta, .. } => meta,
+		}
+	}
+}
+
+impl Serialize for DateValue {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: Serializer,
+	{
+		let meta = self.meta().clone();
+		let mut internal = DateInternal {
+			season: meta.season,
+			circa: meta.circa,
+			literal: meta.literal,
+			extra: meta.extra,
+			..Default::default()
+		};
+
+		match self {
+			Self::Single { date, .. } => {
+				internal.date_parts = vec![date.clone()];
+			}
+			Self::Range { start, end, .. } => {
+				internal.date_parts = vec![start.clone(), end.clone()];
+			}
+			Self::Raw { date, .. } => {
+				internal.raw = Some(date.clone());
+			}
+			Self::Edtf { date, .. } => {
+				internal.edtf = Some(date.clone());
+			}
+		}
+
+		internal.serialize(serializer)
+	}
+}
+
+impl<'de> Deserialize<'de> for DateValue {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+		D::Error: serde::de::Error,
+	{
+		let internal = DateInternal::deserialize(deserializer)?;
+
+		if internal.date_parts.len() == 1 {
+			Ok(Self::Single {
+				date: internal.date_parts[0].clone(),
+				meta: DateMeta::from_internal(internal),
+			})
+		} else if internal.date_parts.len() == 2 {
+			Ok(Self::Range {
+				start: internal.date_parts[0].clone(),
+				end: internal.date_parts[1].clone(),
+				meta: DateMeta::from_internal(internal),
+			})
+		} else if let Some(date) = &internal.edtf {
+			Ok(Self::Edtf {
+				date: date.clone(),
+				meta: DateMeta::from_internal(internal),
+			})
+		} else if let Some(date) = &internal.raw {
+			Ok(Self::Edtf {
+				date: date.clone(),
+				meta: DateMeta::from_internal(internal),
+			})
+		} else {
+			Err(D::Error::custom("unknown date format".to_string()))
+		}
+	}
 }
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
@@ -75,6 +180,17 @@ pub struct DateMeta {
 	/// Date fields not defined above.
 	#[serde(flatten)]
 	pub extra: HashMap<String, OrdinaryValue>,
+}
+
+impl DateMeta {
+	fn from_internal(internal: DateInternal) -> Self {
+		Self {
+			season: internal.season,
+			circa: internal.circa,
+			literal: internal.literal,
+			extra: internal.extra,
+		}
+	}
 }
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
